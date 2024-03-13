b6769109aa60d4fa786f795331e332f1e6465e02
MAXIME Théo <theomax13@hotmail.fr>
2024-03-13 11:49:17 +0100

    fix: animation + glitch into wall

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,10 +1,6 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using Random = UnityEngine.Random;
-using UnityEngine.UI;
-using TMPro;
 
 public class Spawner : MonoBehaviour
 {
@@ -15,79 +11,55 @@ public class Spawner : MonoBehaviour
     [SerializeField] private int enemiesPerRound = 5;
     [SerializeField] private TextMeshProUGUI roundText;
     [SerializeField] private TextMeshProUGUI enemyleftText;
-    private bool isFirstRound = true;
-    private int totalEnemiesToSpawn;
-    private int totalEnemiesSpawnedThisRound;
     private int enemiesRemainingToSpawn;
+    private bool isFirstRound = true;
 
     private float lastSpawnTime;
     private int spawnCount;
+    private int totalEnemiesSpawnedThisRound;
+    private int totalEnemiesToSpawn;
 
     private void Start()
-{
-    PrepareNextRound();
-}
-
-private void Update()
-{
-    if (lastSpawnTime >= spawnInterval && enemiesRemainingToSpawn > 0)
-    {
-        lastSpawnTime = 0;
-        spawnCount++;
-        enemiesRemainingToSpawn--;
-        totalEnemiesSpawnedThisRound++;
-        
-        GameObject chosenPrefab = Random.Range(0, 2) == 0 ? entityPrefab : entityPrefab2;
-        var instance = Instantiate(chosenPrefab, transform.position, Quaternion.identity);
-        var enemyScript = instance.GetComponent<Enemy>();
-        if (enemyScript != null)
-        {
-            enemyScript.Initialize(this); 
-        }
-    }
-    lastSpawnTime += Time.deltaTime;
-
-    // Vérifier si tous les ennemis de la manche ont été générés et éliminés
-    if (totalEnemiesSpawnedThisRound >= totalEnemiesToSpawn && spawnCount <= 0)
     {
         PrepareNextRound();
     }
-}
 
-private void PrepareNextRound()
-{
-    if (!isFirstRound)
+    private void Update()
     {
-        currentRound++;
-    }
-    if (isFirstRound)
-    {
-        isFirstRound = false;
-    }
-    totalEnemiesToSpawn = enemiesPerRound * currentRound;
-    enemiesRemainingToSpawn = totalEnemiesToSpawn;
-    totalEnemiesSpawnedThisRound = 0;
-    if (roundText != null)
-    {
-        roundText.text = "MANCHE " + currentRound;
-    }
-    Debug.Log("Manche " + currentRound);
-    if (enemyleftText != null)
-    {
-        enemyleftText.text = "Ennemis restants: " + spawnCount;
-    }
-    
-    
-}
-public void EnemyDestroyed()
-{
-    spawnCount--;
-    if (enemyleftText != null)
-    {
-        enemyleftText.text = "Ennemis restants: " + spawnCount;
-    }
-    Debug.Log("Ennemi détruit, Restant: " + spawnCount);
-}
+        if (lastSpawnTime >= spawnInterval && enemiesRemainingToSpawn > 0)
+        {
+            lastSpawnTime = 0;
+            spawnCount++;
+            enemiesRemainingToSpawn--;
+            totalEnemiesSpawnedThisRound++;
 
+            var chosenPrefab = Random.Range(0, 2) == 0 ? entityPrefab : entityPrefab2;
+            var instance = Instantiate(chosenPrefab, transform.position, Quaternion.identity);
+            var enemyScript = instance.GetComponent<Enemy>();
+            if (enemyScript != null) enemyScript.Initialize(this);
+        }
 
+        lastSpawnTime += Time.deltaTime;
+
+        if (totalEnemiesSpawnedThisRound >= totalEnemiesToSpawn && spawnCount <= 0) PrepareNextRound();
+    }
+
+    private void PrepareNextRound()
+    {
+        if (!isFirstRound) currentRound++;
+        if (isFirstRound) isFirstRound = false;
+        totalEnemiesToSpawn = enemiesPerRound * currentRound;
+        enemiesRemainingToSpawn = totalEnemiesToSpawn;
+        totalEnemiesSpawnedThisRound = 0;
+        if (roundText != null) roundText.text = "MANCHE " + currentRound;
+        Debug.Log("Manche " + currentRound);
+        if (enemyleftText != null) enemyleftText.text = "Ennemis restants: " + spawnCount;
+    }
+
+    public void EnemyDestroyed()
+    {
+        spawnCount--;
+        if (enemyleftText != null) enemyleftText.text = "Ennemis restants: " + spawnCount;
+        Debug.Log("Ennemi détruit, Restant: " + spawnCount);
+    }
 }
